9128a383044ec214d988ff3706a2576df60d2477
Kylin <1159469891@qq.com>
2025-01-07 15:20:49 +0800

    init: 项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,88 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+
+import path from 'path';
+const pathSrc = path.resolve(__dirname, 'src');
+const resolve = (dir: string) => path.join(__dirname, dir);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon'
+        })
+      ],
+
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts')
+    }),
+    Components({
+      resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep']
+        }),
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver()
+      ],
+
+      dts: path.resolve(pathSrc, 'components.d.ts')
+    }),
+
+    Icons({
+      // 自动安装图标库
+      autoInstall: true
+    })
+  ],
+  //这里进行配置别名
+  resolve: {
+    alias: {
+      '@': pathSrc // @代替src
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      less: {
+        modifyVars: {
+          // 用于全局导入，以避免需要单独导入每个样式文件。
+          // reference:  避免重复引用
+          hack: `true; @import (reference) "${resolve(
+            'src/assets/css/index.less'
+          )}";`
+        },
+        javascriptEnabled: true
+      }
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue', 'pinia', 'vue-router']
+        },
+        chunkFileNames: 'static/js/[name]-[hash].js',
+        entryFileNames: 'static/js/[name]-[hash].js',
+        assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
+      }
+    }
+  }
+});
