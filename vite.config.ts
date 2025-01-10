f3a1102a1b50f4816a1975cbd1cf5574e7bfa575
Kylin <1159469891@qq.com>
2025-01-10 15:19:29 +0800

    refactor: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ import path from 'path';
 const pathSrc = path.resolve(__dirname, 'src');
 const resolve = (dir: string) => path.join(__dirname, dir);
 
+const fePort = 1119;
+const serverOrigin = 'https://192.168.0.248';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -53,6 +56,21 @@ export default defineConfig({
       autoInstall: true
     })
   ],
+  // 服务器设置
+  server: {
+    cors: true, // 默认启用并允许·任何源
+    host: '0.0.0.0', // 指定服务器主机名
+    port: fePort, // 指定服务端口号
+    open: true, // 运行自动打开浏览器
+    strictPort: true, // 若3030端口被占用,直接结束项目
+    proxy: {
+      '/api': {
+        target: serverOrigin,
+        changeOrigin: true,
+        secure: false // 忽略自签名证书
+      }
+    }
+  },
   //这里进行配置别名
   resolve: {
     alias: {
